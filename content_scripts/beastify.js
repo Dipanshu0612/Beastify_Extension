0648103c2473f9df0b26d5cc985f710efdabc6be
Dipanshu Mishra <dipanshu.a.mishra06@gmail.com>
2025-01-29 09:47:45 +0530

    Adding Files

---
+++ content_scripts/beastify.js
@@ -0,0 +1,30 @@
+(() => {
+  if (window.hasRun) {
+    return;
+  }
+  window.hasRun = true;
+
+  function insertBeast(beastURL) {
+    removeExistingBeasts();
+    const beastImage = document.createElement("img");
+    beastImage.setAttribute("src", beastURL);
+    beastImage.style.height = "100vh";
+    beastImage.className = "beastify-image";
+    document.body.appendChild(beastImage);
+  }
+
+  function removeExistingBeasts() {
+    const existingBeasts = document.querySelectorAll(".beastify-image");
+    for (const beast of existingBeasts) {
+      beast.remove();
+    }
+  }
+
+  browser.runtime.onMessage.addListener((message) => {
+    if (message.command === "beastify") {
+      insertBeast(message.beastURL);
+    } else if (message.command === "reset") {
+      removeExistingBeasts();
+    }
+  });
+})();
